93215e81a7d426c6f7b0037592649624b2f97b04
Eugene Petrenko <me@jonnyzzz.com>
2019-11-16 20:24:55 +0000

    working prototype

--- traffic-leds/LED.py
+++ traffic-leds/LED.py
@@ -7,8 +7,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(True)
 
 PIN_R = 16
-PIN_Y = 20
-PIN_G = 21
+PIN_Y = 21
+PIN_G = 20
+
+PIN_A = 25
+PIN_B = 8
+PIN_C = 7
+PIN_D = 12
 
 PIN_ALL = [PIN_R, PIN_Y, PIN_G]
 
@@ -16,16 +21,71 @@ GPIO.setup(PIN_R, GPIO.OUT)
 GPIO.setup(PIN_Y, GPIO.OUT)
 GPIO.setup(PIN_G, GPIO.OUT)
 
-print("LED ready")
+GPIO.setup(PIN_A, GPIO.OUT)
+GPIO.setup(PIN_B, GPIO.OUT)
+GPIO.setup(PIN_C, GPIO.OUT)
+GPIO.setup(PIN_D, GPIO.OUT)
+
+GPIO.output(PIN_A, GPIO.HIGH)
+GPIO.output(PIN_B, GPIO.HIGH)
+GPIO.output(PIN_C, GPIO.HIGH)
+GPIO.output(PIN_D, GPIO.HIGH)
+
+GPIO.output(PIN_R, GPIO.HIGH)
+GPIO.output(PIN_Y, GPIO.HIGH)
+GPIO.output(PIN_G, GPIO.HIGH)
+
+# time.sleep(900009)
+print("LED ready!")
+
+long = 3
+short = long / 8
+
+def red():
+    GPIO.output(PIN_R, GPIO.HIGH)
+    GPIO.output(PIN_Y, GPIO.LOW)
+    GPIO.output(PIN_G, GPIO.LOW)
+    time.sleep(long)
+
+
+def red_yellow():
+    GPIO.output(PIN_R, GPIO.HIGH)
+    GPIO.output(PIN_Y, GPIO.HIGH)
+    GPIO.output(PIN_G, GPIO.LOW)
+    time.sleep(long)
+
+
+def yellow():
+    GPIO.output(PIN_R, GPIO.LOW)
+    GPIO.output(PIN_Y, GPIO.HIGH)
+    GPIO.output(PIN_G, GPIO.LOW)
+    time.sleep(short)
+
+
+def dark():
+    GPIO.output(PIN_R, GPIO.LOW)
+    GPIO.output(PIN_Y, GPIO.LOW)
+    GPIO.output(PIN_G, GPIO.LOW)
+    time.sleep(short)
+
+
+def green():
+    GPIO.output(PIN_R, GPIO.LOW)
+    GPIO.output(PIN_Y, GPIO.LOW)
+    GPIO.output(PIN_G, GPIO.HIGH)
+    time.sleep(long)
+
 
 while True:
-   for pin in PIN_ALL:
-       GPIO.output(pin, GPIO.HIGH)
-       time.sleep(1)
+    red()
+    red_yellow()
+    green()
 
-   for pin in PIN_ALL:
-       GPIO.output(pin, GPIO.LOW)
-       time.sleep(1)
+    for _ in [1, 2, 3]:
+        yellow()
+        dark()
+
+
 
 
 
